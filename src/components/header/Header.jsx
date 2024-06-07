ba34c6132115c274303ffc17cd7c5075297b90b0
Arthur <arturo.kudabaev@mail.ru>
2024-06-08 00:36:34 +0500

    Logics error

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,18 +3,40 @@ import './header.css';
 
 export default class Header extends React.Component{
 
+    state = {
+        error: null,
+    }
+
+    changeError = (message) => {
+        this.setState(({error}) => {
+            const newError = message;
+            return {
+                ...error,
+                error: newError,
+            }
+        })
+    }
+
     render() {
         const {onChangeInput, addTodo, inputChangeLabel} = this.props;
+        const {error} = this.state;
         const addTodoClick = () => {
             if (inputChangeLabel.trim() !== '') {
                 addTodo(inputChangeLabel)
                 onChangeInput('')
+            }else {
+                this.changeError('ww')
             }
         }
         const onKeyPressHandler = (e) => {
+            this.changeError(null)
             if (e.charCode === 13) {
                 addTodoClick();
             }
+        }
+        let classError = 'new-todo';
+        if (error) {
+            classError += ' error-border'
         }
         return (
             <header className="header">
@@ -23,10 +45,11 @@ export default class Header extends React.Component{
                     value={inputChangeLabel}
                     onKeyPress={onKeyPressHandler}
                     onChange={(e) => onChangeInput(e.currentTarget.value)}
-                    className="new-todo"
+                    className={classError}
                     type="text"
                     placeholder="What needs to be done?"
                     autoFocus/>
+                {error && <div className="error">Поле не должно быть пустым</div>}
             </header>
         )
     }
